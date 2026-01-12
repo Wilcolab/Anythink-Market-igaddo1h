3b954f8f253535ef0c93c1b9573d8008e4a25d1b
WilcoApp <hello@wilco.gg>
2026-01-12 20:02:45 +0000

    Initial commit

--- basic_prompt.js
+++
@@ -1,8 +0,0 @@
-function toCamelCase(str) {
-    return str.replace(/([a-z])([a-z]*)/gi, (match, first, rest, offset) => {
-        return offset === 0 ? match.toLowerCase() : first.toUpperCase() + rest.toLowerCase();
-    });
-}
-
-// Example usage:
-console.log(toCamelCase("vedisvigorous")); // vedIsVigorous
